8ec183b455d4cba7c9cb455ecb272b60c70032cf
Gamepat <phyrofreaky@gmail.com>
2019-11-09 18:41:56 +0100

    Added data file

---
+++ src/mqtt/data.h
@@ -0,0 +1,10 @@
+#pragma once
+
+const char* ssid = "Your SSID";
+const char* password = "Your Password";
+
+
+// Broker Info
+const char* mqtt_server = "test.mosquitto.org";
+
+const char* rgbTopic = "Your Topic"
