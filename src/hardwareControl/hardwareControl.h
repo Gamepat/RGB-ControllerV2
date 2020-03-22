f9f04dbd26efd684f4dac59782d17c6ce97e009d
Gamepat <phyrofreaky@gmail.com>
2020-03-22 12:47:11 +0100

    Changed pin definition to real GPIO pins

--- src/hardwareControl/hardwareControl.h
+++ src/hardwareControl/hardwareControl.h
@@ -1,8 +1,12 @@
 #pragma once
 
 #include <Arduino.h>
+#include <util/types.h>
 
-//TODO change to real pins
-#define RED_PIN 0
-#define GREEN_PIN 1
-#define BLUE_PIN 2
+
+#define RED_PIN 0       // GPIO 0
+#define GREEN_PIN 2     // GPIO 2
+#define BLUE_PIN 3      // GPIO 3
+
+
+void pwmControl(rgb_uint8_t values);
